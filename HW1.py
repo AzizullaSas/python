f73ef7a76f4979c5a0f00475ba70227e22018956
Aziz <azizsafihulla@gmail.com>
2025-10-28 18:55:34 +0600

    Home Work #1 Python DS

---
+++ HW1.py
@@ -0,0 +1,117 @@
+#Task 1
+
+number1 = 10
+number2 = 20
+num1 = number1 + number2  
+
+print(num1)
+
+#Task 2
+
+number1 = 17
+number2 = 5
+num2 = number1 // number2
+
+print(num2)
+
+#Task 3 
+
+number1 = 17
+number2 = 5
+num3 = number1 % number2
+
+print(num3)
+#Task 4 
+
+float_num1 = 5.5
+float_num2 = 2.0
+float_num3 = 3.0
+num4 = (float_num1 * float_num2) / float_num3
+
+print(num4)
+#Task 5
+
+num = 4.0
+num5 = round((num ** 3) / 2, 1)
+
+print(num5)
+#Task 6
+
+import math as mt
+number1 = 10.8
+number2 = 3.2
+num6 = mt.floor(number1 - number2)
+
+print(num6)
+#Task 7
+
+number1 = 10.2
+number2 = 3.8
+num7 = mt.ceil(number1 - number2)
+
+print(num7)
+
+#Task 8
+
+a = 3.0
+b = 4.0
+c = mt.sqrt(a**2 + b**2)
+num8 = c
+
+print(num8)
+#Task9 
+
+pos_num = 5
+neg_num = -7
+num9 = abs(pos_num) + abs(neg_num)
+
+print(num9)
+
+#Task 10
+
+temp = 90
+num10 = round((temp - 32) * 5/9, 2)
+
+print(num10)
+
+#Task 11
+
+num11 = ((6 * (7/12) - 3 * (17/36)) * 2.5 - 4 * (1/4) * 0.65) / (4 * (1/4) - 0.5)
+
+print(num11)
+
+#Task 12
+
+num12 = ((2 + 3/4) * 1.1 + 3 * (1/3) * (1/5) * (7/7) - (2 + 1/6) * 4.5 * 0.375) / ((2.5 - 0.4 * 3 * (1/3)) - (2.75 - 1 * (1/2)))
+
+print(num12)
+
+#Task 12
+
+num13 = (11 + 2/5) + (7 + 1/2) * (285.6 / 14 - 1 * (23/30) + (13/50)) * (24.4 - 10.23)
+
+print(num13)
+
+#Task 13
+
+num13 = (11 + 2/5) + (7 + 1/2) * (285.6 / 14 - 1 * (23/30) + (13/50)) * (24.4 - 10.23)
+
+print(num13)
+
+# Task 14
+
+num14 = ((9 - 5 * (3/8)) * (4 + 5/12 - 4 * 2 * (2/3)) + ((3/10) - 0.5 * 4) * (4/7)) / ((1/24) + 0.25 * 13 * (1/3))
+
+print(num14)
+
+#Task 15
+
+num15 = 5.75 / 0.025
+
+print(num15)
+
+#Task 16
+
+num16 = ((0.16 * (3.2 - 3/40) + 2 * (3/11) * 4.125 / (3 * (3/4))) / (5 * (1/6) * 0.3 - 0.3 * 4.5 + (1/3) * 0.3)) * 0.4
+
+print(num16)
