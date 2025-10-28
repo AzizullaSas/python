b15dc9a9202e9a9f9e6c3b4725954d0a7df46784
Aziz <azizsafihulla@gmail.com>
2025-10-28 18:39:00 +0600

    27 Oct

---
+++ Fourthlesson.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5]
+# students = ["Tom", "Sam", "Bob", "kane", "kirill"]
+# students_list = [students[4]] * 10
+# students[2] = "Aziz"
+# len_list = len(students)
+# print(len_list)
+
+student1 = ["Tom", "Sam", "Bob",]
+student1.append("Tema") # Добавляет элемент в конец списка
+names2 = student1 [:]
+numbers1 = numbers [::-3]
+numbers1.reverse
+print (names2)
+print(numbers1)
+
+
+
+
---
+++ library.py
@@ -0,0 +1,46 @@
+import math as mt
+# # import pandas
+# num1 = 4.4
+# num2 = round(num1)
+# num3 = mt.floor(num1)
+# num4 = mt.ceil(num1)
+# num5 = mt.radians(30)
+# num6 = mt.degrees(0.5235987755982988)
+# num7 = mt.pow(3, 2) # ** - степень
+# num8 = mt.sqrt(81) # 81 ** 0.5
+# print(num8)
+# exit() # - Благодрая ей можно запускать только часть кода.
+# # print(math.ceil(num1))
+
+
+# name1, name2, name3 = "Aziz", "Afgan", "Azizulla"
+# print(name3)
+
+
+# print (max(2, 4, 6, 7, 10, 25, 400))
+# print (min(1 , 405005, 20, 2 , 2.00001))
+
+
+# x = 10
+# y = 20
+# z = 30
+# v = x * y * z
+# print (v)
+
+# r = 3
+# h = 7
+# v = (mt.pi * r**2 * h) / 3
+# print (v)
+
+# r = 10
+# h = 6
+# v = (mt.pi * r**2 * h)
+# print(v)
+
+# a = 4 / 3
+# r = 10 
+# v = (a * mt.pi * r**3 )
+# print(v)
+
+
+
