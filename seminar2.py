8265a2546585761a51b35e37cafce2b1957c9e2b
Aziz <azizsafihulla@gmail.com>
2025-10-29 19:15:46 +0600

    Seminar2 HomeWork

---
+++ seminar2.py
@@ -0,0 +1,90 @@
+# #Task 1
+num = int(input("Enter the number:"))
+if num > 5:
+    print(True)
+else:
+    print(False)
+    
+ #Task 2
+num1 = int(input("Enter the number please:"))
+if num1 % 2!= 00:
+    print("Нечетное")
+else:
+    print("Четное")
+    
+# # Task 3
+
+a = int(input("Enter the number a :"))
+b = int(input("Enter the number b :"))
+
+if a > b * 2:
+    print("Correct")
+else:
+    print("incorrect")
+
+# #Task 4
+
+num2 = int(input("Enter the number:"))
+
+if num2 % 3 == 0 :
+    print("Yes")
+else:
+    print("No")
+
+
+# Task 5
+
+num3 = int(input("Enter the number: "))
+
+if num3 % 2!= 00:
+    print(f"{num3 + 1}")
+else:
+    print(f"{num3}")
+
+
+# Task 6
+
+x = int(input("Enter the number:"))
+if x > 3 and x <= 8:
+    print("Yes")
+else:
+    print("No")
+
+
+# Task 7
+
+x = int(input("Enter the number:"))
+if x >= 5 and x < 15 and x != 10:
+    print("Yes")
+else:
+    print("No")
+
+# Task 8
+
+x =  int(input("Enter the number: "))
+
+if x <= 5 or x > 10:
+    print("Yes")
+else:
+    print("No")
+    
+    
+#Task 9 
+
+x = int(input("Enter the number: "))
+
+if (2 < x <= 6) or x > 10:
+    print("Yes")
+else:
+    print("No")
+
+
+#Task 10
+
+x = int(input("Enter the number: "))
+
+if x < 4 or x > 10 or x <= 2 or x >= 6:
+    print("Yes")
+else:
+    print("No")
+    
